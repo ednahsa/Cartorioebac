7ac03563c2a0ff3f6734f29fca9c2b406d219a06
ednahsa <ednahsa_@outlook.com>
2024-05-28 19:50:50 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário

--- md4.c
+++ md4.c
@@ -133,6 +133,7 @@ int main()
 	printf("\t1 - Registrar nomes\n");
 	printf("\t2 - Consultar nomes\n");
 	printf("\t3 - Deletar nomes\n");
+	printf("\t4 - Sair do sistema\n\n");
 	printf("Opção: "); //Fim do menu
 	
 	scanf("%d", &opcao); //armazenamento e a escolha do usuário
@@ -154,6 +155,10 @@ int main()
 	deletar();
 	break;
 	
+	case 4:
+	printf("Obrigado por utilizar o sistema!\n");
+	return 0;
+	break;
 	
 	default:	
 	printf("essa opção não está disponivel!\n");
